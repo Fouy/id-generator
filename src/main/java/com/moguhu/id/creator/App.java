56eb0a035b5b6ff10e94fe452bf5a86fe1be5759
xuefeihu <2252603873@qq.com>
2016-07-03 10:34:23 +0800

    hello world!

---
+++ src/main/java/com/moguhu/id/creator/App.java
@@ -0,0 +1,13 @@
+package com.moguhu.id.creator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
